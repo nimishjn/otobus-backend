a87bcc0b6f62dc71f5ac9627edbf8229e0f5b3aa
agtarunika <tarunika1607@gmail.com>
2021-12-05 01:10:30 +0530

    feat: Id Generator

---
+++ scripts/idGenerator.js
@@ -0,0 +1,21 @@
+function busIdGenerator(length) {
+    let result = 'BUS';
+    const characters = '0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function bookingIdGenerator(length) {
+    let result = 'BOOK';
+    const characters = '0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+export default { bookingIdGenerator, busIdGenerator };
