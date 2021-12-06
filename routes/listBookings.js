ad0ce5d3ad6125e5de785a268a90dfb39a0d30f2
nimishjn <nimishjain100701@gmail.com>
2021-12-06 23:43:52 +0530

    feat: listBookings route

---
+++ routes/listBookings.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Booking = require("../models/Booking");
+
+router.get("/", (req, res, next) => {
+  Booking.find({email: "abc@gmail.com"})
+    .exec()
+    .then((bookings) => {
+      return res.status(200).json({
+        code: "S2",
+        bookings: bookings,
+      });
+    });
+});
+
+module.exports = router;
